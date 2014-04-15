4160fe5a0dfdf30a705e0b4cbbcc1f360ef39fda
ct2034 <post@henkelchristian.de>
2014-04-15 14:59:18 +0200

    improved testing setup

---
+++ build/CMakeFiles/rostest_launch_my_test_2.test.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/rostest_launch_my_test_2.test"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/rostest_launch_my_test_2.test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ build/CMakeFiles/rostest_launch_my_test_2.test_result.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/rostest_launch_my_test_2.test_result"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/rostest_launch_my_test_2.test_result.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
